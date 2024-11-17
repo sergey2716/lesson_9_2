2d2af425f7198be22409a2a5970605a1b439f869
sergey2716 <serge-avva@mail.ru>
2024-11-17 09:51:40 +0500

    Update module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -29,5 +29,6 @@ second_result = [(x,y)  for x in first_strings  for y in second_strings if len(x
 third_result = {x: len(x) for x in first_strings + second_strings if len(x) % 2 == 0}
 
 print(first_result)
-print(second_strings)
+print(second_result)
 print(third_result)
+
